0084f6c0b034856ed71e29235af27c36807c529f
vikassrini <1rn20cs178.vikassrinivasa@gmail.com>
2025-02-13 21:51:21 +0530

    Update App.tsx

--- root/App.tsx
+++ root/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
