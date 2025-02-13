315fb6391850b5f28cf0cab3d9cbad655d316280
vikassrini <1rn20cs178.vikassrinivasa@gmail.com>
2025-02-13 21:24:09 +0530

    Update App.tsx

--- root/App.tsx
+++ root/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
